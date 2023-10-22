a1e45e0ee09abb03663fbcc2bda62d6279852734
Aidan <aidansosnowski@gmail.com>
2023-10-21 22:02:21 -0500

    Update #3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-import { analyzeImage } from './azure-image-analysis'; // Import the analyzeImage function
+import { analyzeImage } from './azure-image-analysis';
 import dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config(); // Load environment variables from .env
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [result, setResult] = useState('');
+  const [imageUrl, setImageUrl] = useState(''); // State to store the image URL
 
   const handleAnalyzeClick = async () => {
     if (inputValue) {
       try {
         const analysisResult = await analyzeImage(inputValue);
         setResult(JSON.stringify(analysisResult, null, 2));
+        setImageUrl(inputValue); // Set the image URL for display
       } catch (error) {
         setResult(`Error: ${error.message}`);
       }
@@ -24,6 +26,7 @@ function App() {
   const handleGenerateClick = () => {
     // Perform image generation here and update the result
     setResult(`Generating image from: ${inputValue}`);
+    setImageUrl(''); // Clear the image URL when generating
   };
 
   return (
@@ -45,9 +48,11 @@ function App() {
       <button onClick={handleGenerateClick}>Generate Image</button>
 
       <div id="result">{result}</div>
+
+      {/* Display the image if imageUrl is not empty */}
+      {imageUrl && <img src={imageUrl} alt="Analyzing Image" />}
     </div>
   );
 }
 
 export default App;
-
