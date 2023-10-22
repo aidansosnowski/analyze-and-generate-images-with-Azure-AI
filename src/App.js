99c4bca81738d3207b3d7d5b9d17811d078d7751
Aidan <aidansosnowski@gmail.com>
2023-10-21 21:06:03 -0500

    Update Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleAnalyzeClick = () => {
+    // Perform image analysis here and update the result
+    setResult(`Analyzing image: ${inputValue}`);
+  };
+
+  const handleGenerateClick = () => {
+    // Perform image generation here and update the result
+    setResult(`Generating image from: ${inputValue}`);
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation</h1>
+
+      <label htmlFor="inputField">Enter URL or Prompt:</label>
+      <input
+        type="text"
+        id="inputField"
+        placeholder="Enter URL or Prompt"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleAnalyzeClick}>Analyze Image</button>
+      <button onClick={handleGenerateClick}>Generate Image</button>
+
+      <div id="result">{result}</div>
+    </div>
+  );
 }
 
 export default App;
